f9dde8044fc346c910efd1591d788aa7bb6e9ae2
Mohammed Rashid <rashid_92@hotmail.co.uk>
2016-09-19 09:44:52 +0100

    add 90's wall

--- js/app.js
+++ js/app.js
@@ -17,6 +17,10 @@ $('#three').on("click", function() {
     // Animation complete.
   });
 });
+// $('#super-mario').on("click", function() {
+     
+// });
+
 
 
 $.ajax({
@@ -31,30 +35,27 @@ $.ajax({
     $("#giphy").attr("src", response.data.image_url);
         console.log(response.data.image_url);
         
-          $.ajax({
-            url: "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=american+psycho",
-            type: 'GET',
-            async: true,
-            headers: { 
-                Accept : "image/jpg",
-                "Content-Type": "application/json"
-            },
-            success: function(response) {
-            $("#giphy").attr("src", response.data.image_url);
-                console.log(response.data.image_url);
-            },
-            error: function(){
-              alert("Cannot get search data");
-            }
-        });
+        //   $.ajax({
+        //     url: "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=american+psycho",
+        //     type: 'GET',
+        //     async: true,
+        //     headers: { 
+        //         Accept : "image/jpg",
+        //         "Content-Type": "application/json"
+        //     },
+        //     success: function(response) {
+        //     $("#giphy").attr("src", response.data.image_url);
+        //         console.log(response.data.image_url);
+        //     },
+        //     error: function(){
+        //       alert("Cannot get search data");
+        //     }
+        // });
     },
     error: function(){
       alert("Cannot get data");
     }
 });
-
-
-
 
 });
 
