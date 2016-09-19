1edb0b60f868040263f35f3ede981f4d7ab11258
Mohammed Rashid <rashid_92@hotmail.co.uk>
2016-09-19 01:37:50 +0100

    finish random giphy ajax request and add/style game wall

--- js/app.js
+++ js/app.js
@@ -1,26 +1,61 @@
  $(function(){
 
+$('#one').on("click", function() {
+      $( "#sec-one" ).toggle( "slow", function() {
+    // Animation complete.
+  });
+});
+
+$('#two').on("click", function() {
+      $( "#sec-two" ).toggle( "slow", function() {
+    // Animation complete.
+  });
+});
+
+$('#three').on("click", function() {
+      $( "#sec-three" ).toggle( "slow", function() {
+    // Animation complete.
+  });
+});
+
+
 $.ajax({
     url: "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=american+psycho",
+    type: 'GET',
     async: true,
-        headers: { 
+    headers: { 
         Accept : "image/jpg",
         "Content-Type": "application/json"
     },
-    type: 'GET',
     success: function(response) {
     $("#giphy").attr("src", response.data.image_url);
         console.log(response.data.image_url);
-     },
+        
+          $.ajax({
+            url: "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=american+psycho",
+            type: 'GET',
+            async: true,
+            headers: { 
+                Accept : "image/jpg",
+                "Content-Type": "application/json"
+            },
+            success: function(response) {
+            $("#giphy").attr("src", response.data.image_url);
+                console.log(response.data.image_url);
+            },
+            error: function(){
+              alert("Cannot get search data");
+            }
+        });
+    },
     error: function(){
       alert("Cannot get data");
     }
-}).done(function(data){
-  
 });
 
 
 
 
 });
+
 
